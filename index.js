ed235a7c4814500f053266d123800540167b9d77
Pranay Shinde <PranayShinde486@gmail.com>
2025-03-10 23:21:17 +0530

    show Post and unique id using uuidv4

--- index.js
+++ index.js
@@ -3,40 +3,52 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
+const { v4: uuidv4 } = require('uuid');
 app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 
 app.use(express.static(path.join(__dirname, "public")));
 
-let posts =[
+let posts = [
     {
-        username :"Pranay",
+        id: uuidv4(),
+        username: "Pranay",
         content: "I Love Coding"
     },
     {
-        username :"Pranay1",
+        id: uuidv4(),
+        username: "Pranay1",
         content: "I also Love Coding"
     }
 ]
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("working")
 })
 
-app.get("/posts",(req,res)=>{
-    res.render("index.ejs",{posts})
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts })
 })
 
-app.get("/posts/new",(req,res)=>{
+app.get("/posts/new", (req, res) => {
     res.render("new.ejs")
-     
+
 })
 
-app.post("/posts",(req,res)=>{
-    
-    let {username,content }= req.body
-    posts.push({username,content})
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id == p.id);
+    console.log(post);
+    res.render("show.ejs", { post });
+
+})
+
+app.post("/posts", (req, res) => {
+
+    let { username, content } = req.body
+    let id = uuidv4()
+    posts.push({ id,username, content })
     res.redirect("/posts")
 })
 
