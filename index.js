1fa123b91ad9ab72e2b7d2f3a063eae53a3179e0
Pranay Shinde <PranayShinde486@gmail.com>
2025-03-11 23:22:15 +0530

    RESTful Complete CRUD

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const methodOverride = require("method-override");
+
+
 
 const { v4: uuidv4 } = require('uuid');
 app.use(express.urlencoded({ extended: true }))
@@ -9,7 +12,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(methodOverride("_method"))
 let posts = [
     {
         id: uuidv4(),
@@ -48,8 +51,32 @@ app.post("/posts", (req, res) => {
 
     let { username, content } = req.body
     let id = uuidv4()
-    posts.push({ id,username, content })
+    posts.push({ id, username, content })
     res.redirect("/posts")
+})
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts")
+})
+
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts")
+})
+
+
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", { post })
+
 })
 
 app.listen(port, () => {
